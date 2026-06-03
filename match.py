655dbde801ca6f0721897603c66c6d7d9588bce2
Jonathan Krolak <jkrolak@adriansteel.com>
2026-06-03 15:16:05 -0400

    Use Elo-style probabilities and Poisson goal simulation

--- match.py
+++ match.py
@@ -1,39 +1,67 @@
+import math
 import random
 from data import teams
 
 
-def simulate_match(team_one, team_two):
+def calculate_expected_result(team_one, team_two):
     team_one_rating = teams[team_one]
     team_two_rating = teams[team_two]
 
-    rating_difference = team_one_rating - team_two_rating
+    rating_difference = team_two_rating - team_one_rating
 
-    team_one_base_goals = random.randint(0, 2)
-    team_two_base_goals = random.randint(0, 2)
+    expected_result = 1 / (10 ** (rating_difference / 600) + 1)
 
-    team_one_bonus_chance = max(0, rating_difference / 400)
-    team_two_bonus_chance = max(0, -rating_difference / 400)
+    return expected_result
 
-    team_one_goals = team_one_base_goals
-    team_two_goals = team_two_base_goals
 
-    if random.random() < team_one_bonus_chance:
-        team_one_goals += 1
+def generate_poisson_goals(expected_goals):
+    goals = 0
+    probability = math.exp(-expected_goals)
+    cumulative_probability = probability
+    random_value = random.random()
 
-    if random.random() < team_two_bonus_chance:
-        team_two_goals += 1
+    while random_value > cumulative_probability:
+        goals += 1
+        probability *= expected_goals / goals
+        cumulative_probability += probability
+
+    return goals
+
+
+def calculate_expected_goals(team_one, team_two):
+    team_one_expected_result = calculate_expected_result(team_one, team_two)
+
+    rating_advantage = team_one_expected_result - 0.5
+
+    team_one_expected_goals = 1.25 + rating_advantage
+    team_two_expected_goals = 1.25 - rating_advantage
+
+    team_one_expected_goals = max(0.4, min(2.6, team_one_expected_goals))
+    team_two_expected_goals = max(0.4, min(2.6, team_two_expected_goals))
+
+    return team_one_expected_goals, team_two_expected_goals
+
+
+def simulate_match(team_one, team_two):
+    team_one_expected_goals, team_two_expected_goals = calculate_expected_goals(
+        team_one,
+        team_two,
+    )
+
+    team_one_goals = generate_poisson_goals(team_one_expected_goals)
+    team_two_goals = generate_poisson_goals(team_two_expected_goals)
 
     return team_one_goals, team_two_goals
 
+
 def simulate_penalty_shootout(team_one, team_two):
-    team_one_rating = teams[team_one]
-    team_two_rating = teams[team_two]
+    team_one_expected_result = calculate_expected_result(team_one, team_two)
+
+    team_one_make_chance = 0.76 + ((team_one_expected_result - 0.5) * 0.08)
+    team_two_make_chance = 0.76 + (((1 - team_one_expected_result) - 0.5) * 0.08)
 
     team_one_score = 0
     team_two_score = 0
-
-    team_one_make_chance = 0.72 + ((team_one_rating - team_two_rating) / 5000)
-    team_two_make_chance = 0.72 + ((team_two_rating - team_one_rating) / 5000)
 
     for shot in range(5):
         if random.random() < team_one_make_chance:
@@ -52,6 +80,7 @@ def simulate_penalty_shootout(team_one, team_two):
     winner = team_one if team_one_score > team_two_score else team_two
 
     return winner, team_one_score, team_two_score
+
 
 def simulate_knockout_match(team_one, team_two):
     team_one_goals, team_two_goals = simulate_match(team_one, team_two)
